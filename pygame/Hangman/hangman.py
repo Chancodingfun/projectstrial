23f3ddc6215220e1fea2454edff1b4c27d875c34
Chancodingfun <chanminzin5@gmail.com>
2025-09-25 19:24:19 +0630

    hangman.py

--- pygame/Hangman/hangman.py
+++ pygame/Hangman/hangman.py
@@ -15,12 +15,24 @@ for i in range(7):
     image = pygame.image.load("hangman"+str(i)+".png")
     images.append(image)
 
+# button variables
+RADIUS = 20
+GAP = 15
+letters = []
+startx = round(WIDTH-((RADIUS*2+GAP)*13)/2)
+starty = 400
+for i in range(26):
+    x = startx + GAP*2 + ((RADIUS * 2+GAP)*(i % 13))
+    y = starty+((i//13)*(GAP + RADIUS*2))
+    letters.append([x, y])
+
 # game variables
 
 hangman_status = 4
 
 # colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 # setup game loop
 
@@ -28,11 +40,21 @@ FPS = 60
 clock = pygame.time.Clock()
 run = True
 
+
+def draw():
+    win.fill(WHITE)
+    # draw buttons
+    for letter in letters:
+        x, y = letter
+        pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+
+    win.blit(images[hangman_status], (150, 100))
+    pygame.display.update()
+
+
 while run:
     clock.tick(FPS)
-    win.fill(WHITE)
-    win.blit(images[hangman_status], (150.100))
-    pygame.display.update()
+    draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -42,3 +64,5 @@ while run:
 
 
 pygame.quit()
+
+
