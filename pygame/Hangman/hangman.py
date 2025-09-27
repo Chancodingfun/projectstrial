5dfd29afb7c5de47cb54371466f3ba625d2e350d
Chancodingfun <chanminzin5@gmail.com>
2025-09-27 21:17:05 +0630

    Update hangman.py

--- pygame/Hangman/hangman.py
+++ pygame/Hangman/hangman.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import math
 
 # setup display
 
@@ -10,6 +11,7 @@ pygame.display.set_caption("Hangman:Find answer before getting hanged!")
 
 
 # button variables
+
 RADIUS = 20
 GAP = 15
 letters = []
@@ -19,7 +21,11 @@ A = 65
 for i in range(26):
     x = startx + GAP*2 + ((RADIUS * 2+GAP)*(i % 13))
     y = starty+((i//13)*(GAP + RADIUS*2))
-    letters.append([x, y,chr(A+i)])
+    letters.append([x, y, chr(A+i),True])
+
+# fonts
+LETTER_FONT = pygame.font.SysFont("comicscans", 40)
+
 
 # load images
 
@@ -33,6 +39,7 @@ for i in range(7):
 hangman_status = 4
 
 # colors
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
@@ -47,8 +54,11 @@ def draw():
     win.fill(WHITE)
     # draw buttons
     for letter in letters:
-        x, y = letter
-        pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+        x, y, ltr,visible= letter
+        if visible:
+            pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+            text = LETTER_FONT.render(ltr, 1, BLACK)
+            win.blit(text, (x-text.get_width()/2, y-text.get_height()/2))
 
     win.blit(images[hangman_status], (150, 100))
     pygame.display.update()
@@ -60,9 +70,14 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            print(pos)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            m_x,m_y = pygame.mouse.get_pos()
+            for letter in letters:
+                x,y,ltr,visble= letter
+                if visble:
+                    dis= math.sqrt((x-m_x)**2+(y-m_y)**2)
+                    if dis<RADIUS:
+                        letter[3]=False
 
 
 pygame.quit()
